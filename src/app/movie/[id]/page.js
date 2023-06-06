2401f76e59d1a5758137c8847e9324f5f4b796b0
shervan <shervangh@gmail.com>
2023-06-06 21:56:09 +0330

    first commit

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -8,14 +8,14 @@ export async function generateStaticParams() {
     const movies = await getPopularMoveis();
 
     return movies?.results?.map(mov => ({
-        userId: mov?.id
+        userId: mov?.id?.toString()
     }))
     
 }
 
 export default async function page({params:{id}}) {
-    let userId = id.toString();
-    const movieDetails = await getMovie(userId)
+    let userId = id
+    const movieDetails = await getMovie(String(userId))
   return (
     <div className='py-4'>
         <MovieDetail movie={movieDetails}/>
