e4d8a3e49f23e78f05624220859520b6f4259ebc
shervan <shervangh@gmail.com>
2023-06-07 16:13:08 +0330

    first commit

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -1,12 +1,17 @@
 
 import getMovie from '../../../../lib/getMovie';
 import MovieDetail from './components/MovieDetail';
-export default async function page(props) {
-    const id = props.params.id
+import { Suspense } from 'react';
+
+
+export default async function page({params:{id}}) {
     const movieDetails = await getMovie(id)
+    console.log(id,'mooooooooooooovi')
     return (
         <div className='py-4 text-white'>
-        <MovieDetail movie={movieDetails}/>
+            <Suspense fallback={<h2>loading ...</h2>}>
+                <MovieDetail movie={movieDetails}/>
+            </Suspense>
         </div>
     )
 }
