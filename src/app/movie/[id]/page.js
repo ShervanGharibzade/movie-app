161b54e26c1150b51c2571e0bac3dc0369b4628c
shervan <shervangh@gmail.com>
2023-06-07 18:21:44 +0330

    first commit

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -1,21 +1,33 @@
 
 import getMovie from '../../../../lib/getMovie';
+import getPopularMoveis from '../../../../lib/getPopularMoveis';
 import MovieDetail from './components/MovieDetail';
 import { Suspense } from 'react';
 
 
 export default async function page({params:{id}}) {
-    const movieId = id
-    let movieDetails;
-    if(movieId) movieDetails = await getMovie(id);
+    
+    const movieDetails = await getMovie(id);
     
     
-    console.log(movieId,'mooooooooooooovi')
+    console.log(id,'mooooooooooooovi')
     return (
         <div className='py-4 text-white'>
             <Suspense fallback={<h2>loading ...</h2>}>
                 <MovieDetail movie={movieDetails}/>
             </Suspense>
         </div>
+    )
+}
+
+
+export async function generateStaticParams () {
+    const popularMovies = await getPopularMoveis();
+    const movies = await popularMovies;
+    
+    return movies.results.map(
+        movie =>({
+            id: movie.id.toString()
+        })
     )
 }
