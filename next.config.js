fe7d4a9a3955c9ec739555f9de0bba86479f7558
shervan <shervangh@gmail.com>
2023-06-07 23:29:32 +0330

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
   const nextConfig = {
     output: 'export',
-  }
+    async rewrites() {
+      return [
+        { source: "/", destination: "/search/:name" },
+      ];
+    }
+  };
+  
 
 module.exports = nextConfig
