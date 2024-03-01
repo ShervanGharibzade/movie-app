f9717aa24b7ec66a24428769a7df8b1d0d794745
shervan <hosein.g@pirelly.com>
2024-03-01 22:02:07 +0330

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD:next.config.js
   reactStrictMode: true,
-=======
-  output: 'export',
->>>>>>> 491d978b73fd0b462f717a0d3cc274dd27a9ba44:next.config.mjs
+  output: "export",
   images: { unoptimized: true },
 };
 
